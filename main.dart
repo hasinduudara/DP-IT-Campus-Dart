453f20fb003ad3ccb01769a1db0891c48e153428
Hasindu Udara <hasindutechschool@gmail.com>
2025-08-02 11:46:59 +0530

    Add comments and update print statements in main.dart

--- main.dart
+++ main.dart
@@ -1,4 +1,17 @@
 void main() {
-  print("Hello World!");
-  print('Hasindu Udara');
+  // dart comments
+
+  // This is a single-line comment in Dart
+  print("Hello, Dart!"); // This prints a message to the console
+
+  /*
+   * This is a multi-line comment in Dart.
+   * It can span multiple lines.
+   */
+  print("This is a multi-line comment example."); // Another print statement
+
+  /// This is a documentation comment in Dart.
+  ///
+  /// /// It is used to document code and can be processed by tools like dartdoc.
+  /// print("Documentation comments are useful for generating documentation."); // Final print statement
 }
