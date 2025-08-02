77cda0ab96ee7917ce63938e5af609f81c004763
Hasindu Udara <hasindutechschool@gmail.com>
2025-08-02 11:48:24 +0530

    Refactor main.dart to simplify print statements and remove comments

--- main.dart
+++ main.dart
@@ -1,17 +1,4 @@
 void main() {
-  // dart comments
-
-  // This is a single-line comment in Dart
-  print("Hello, Dart!"); // This prints a message to the console
-
-  /*
-   * This is a multi-line comment in Dart.
-   * It can span multiple lines.
-   */
-  print("This is a multi-line comment example."); // Another print statement
-
-  /// This is a documentation comment in Dart.
-  ///
-  /// /// It is used to document code and can be processed by tools like dartdoc.
-  /// print("Documentation comments are useful for generating documentation."); // Final print statement
+  print("100" + "200");
+  print(100 + 200);
 }
