570ced3bccff9b99d7a119fc55b0b4a10067d61f
Hasindu Udara <hasindutechschool@gmail.com>
2025-08-03 10:45:24 +0530

    String Methods deep dive in Dart

--- main.dart
+++ main.dart
@@ -1,21 +1,35 @@
 void main() {
-  // Variables and Data Types Basics
+  // String Methods deep dive in Dart
 
-  // String variable
-  String firtName = "John";
+  // String concatanation
+  // String firstName = "John";
+  // String lastName = "Doe";
+  // String fullName = firstName + " " + lastName;
+  // print(fullName);
 
-  // Integer variable
-  int age = 23;
+  // String interpolation
+  // String greeting = "Hello!";
+  // String name = "Alice";
 
-  // Double variable
-  double height = 5.9;
+  // Hello! My name is Alice.
 
-  // Boolean variable
-  bool isStudent = true;
+  // String msg = "";
+  // msg = "$greeting My name is $name.";
 
-  // Printing variables
-  print("First Name: $firtName");
-  print("Age: $age");
-  print("Height: $height");
-  print("Is Student: $isStudent");
+  // print(msg);
+
+  // String name = "Alice";
+  // int age = 30;
+
+  // My name is Alice and I am 23 years old.
+
+  // String msg = "";
+  // msg = "My name is $name and I am ${age - 7} years old.";
+
+  // print(msg);
+
+  String name = "hasindu udara";
+  print(name.toUpperCase());
+  print(name.length);
+  print(name.replaceAll("hasindu", "udara"));
 }
